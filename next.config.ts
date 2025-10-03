7132ea5df06cd6ef584c3b123929bf16f9a0e9e2
ZxZxZ143 <kyrlukov205@gmail.com>
2025-10-03 18:23:30 +0500

    add desktop

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true, // иначе next/image не заработает на статиках
+  },
 };
 
 export default nextConfig;
